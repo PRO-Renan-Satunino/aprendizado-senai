1ec858dccd99dd29ae9e0cf55cf79d4fb105ade2
PRO-Renan-Satunino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2024-04-08 11:22:30 -0300

    Add files via upload

--- Sem Título1.cpp
+++ Sem Título1.cpp
@@ -1,62 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <string.h>
 
-int main (){
-	
-	int valor, somaGeral = 0, somaPares = 0, contadorGeral = 0;
-	int pares = 0, impares = 0, menorNumero = INT_MAX, maiorNumero = INT_MIN;
-	float mediaGeral, mediaPares;
-	
-	printf("Escreva algum número: ");
-	scanf("%i", &valor);
-	
-	while (valor != 0) {
-		if (valor > 0) {
-			contadorGeral ++;
-			somaGeral += valor;
-			
-			if (valor % 2 == 0) {
-				//pares = pares + 1
-				pares++;
-				
-				//somaPares = somaPares + valor;
-				somaPares += valor;
-			} else {
-				impares++;
-			}
-			
-			if (valor > maiorNumero) {
-				maiorNumero = valor;
-			}
-			
-			if ( valor < menorNumero) {
-				menorNumero = valor;
-			}
+int main() {
+    char email[50];
+    char senha[20];
+    char emailDefinido[50];
+    char senhaDefinida[20];
+    int repeticao = 3;
+
+    printf("Defina seu e-mail: ");
+    scanf("%s", email);
+
+    printf("Defina sua senha: ");
+    scanf("%s", senha);
+
+ 	    	system("cls");
+    	   
+
+    while (repeticao > 0) {
+    
+
+        printf("Digite seu e-mail: ");
+        scanf("%s", emailDefinido);
+
+        printf("Digite sua senha: ");
+        scanf("%s", senhaDefinida);
+        
+        if (strcmp(emailDefinido, email) != 0 || strcmp(senhaDefinida, senha) != 0){
+        	printf("==Acesso Negado==\n\n");
+        	repeticao--;
+		} else {
+			printf("\n==Acesso Concedido==");
+			break;
 		}
-		printf("Digite um numero: ");
-		scanf("%i", &valor);
-		fflush(stdin);
+		
+    } 
+
+    if (repeticao == 0) {
+    	printf("numero maximo de tentativas excedido");
 	}
+    	   
 	
-	if (contadorGeral == 0) {
-		printf(" Nao foi informado um numero positivo.");
-	} else {
-		// cast
-		//2 -> 2.0
-		mediaGeral = somaGeral / (float) contadorGeral;
-		mediaPares = somaPares / (float) pares;
-	
-		printf("Quantidade de Pares: %i \n", pares);
-		printf("Quantidade de impares: %i \n", impares);
-		printf("Media Geral: %.1f \n", mediaGeral);
-		printf("Media de numeros pares: %.1f \n", mediaPares);	
-		printf("\nMaior Numero: %i", maiorNumero);
-		printf("\nMenor Numero: %i", menorNumero);
-	
-	}
-	
-	return 0;
-	 
-	
+    return 0;
 }
+
