d652d40be5ccbda21e658740801cb1bcf56d4684
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2024-04-12 08:26:34 -0300

    Add files via upload

---
+++ paresEimpares.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define TAM 6
+
+int main() {
+	
+		setlocale(LC_ALL, "Portuguese");
+		
+    	int numeros[TAM];
+    	int pares = 0, impares = 0;
+
+    printf("Digite 6 números:\n");
+
+    // Solicita os números ao usuário e armazena no array
+    for (int i = 0; i < TAM; i++) {
+        printf("Número %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+
+    // Verifica se cada número é par ou ímpar e incrementa os contadores
+    for (int i = 0; i < TAM; i++) {
+        if (numeros[i] % 2 == 0) {
+            pares++;
+        } else {
+            impares++;
+        }
+    }
+
+    // Exibe o resultado
+    printf("Quantidade de números pares: %d\n", pares);
+    printf("Quantidade de números ímpares: %d\n", impares);
+
+    return 0;
+}
+
