6355eb4df3040597bb4f05b4292db5ee5e4ea86c
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2024-04-15 08:26:07 -0300

    Add files via upload

--- codigoErrado.cpp
+++ codigoErrado.cpp
@@ -5,21 +5,30 @@
 #define TAM 6
 
 int main (){
-	setlocale (LC_ALL, "portuguese");
-	
-	int numero[TAM];
-	
-	//fazendo cera pro usuario esperar
-	printf("Iniciando Programa...");
-	system ("cls");
-	
-	for (int i = 0; i < TAM; i++){
-		do {
-			printf("Insira o %iº valor: ", i+1);
-			scanf ("%i", &numero[i]);
-		} while (numero[i] % 2 != 0 || numero[i] < 0 || numero[i] != (int)numero[i]);
-	}
-	
-	return 0;
+    setlocale (LC_ALL, "portuguese");
+    
+    int numero[TAM];
+    int i;
+    
+    // Solicita 6 números pares ou inteiros
+    for (i = 0; i < TAM;){
+        printf("Insira o %iº valor (par ou inteiro): ", i+1);
+        scanf ("%i", &numero[i]);
+        
+        // Verifica se o número é par ou inteiro
+        if (numero[i] % 2 == 0 || numero[i] >= 0) {
+            i++; // Incrementa apenas se for par e positivo
+        } else {
+            printf("Número inválido. Insira um número par ou inteiro positivo.\n");
+        }
+    }
+    
+    // Exibição dos números em ordem invertida
+    printf("\n	===Exibindo Resultados===\n\n");
+    for(i = TAM - 1; i >= 0; i--){
+        printf("%iº valor: %i\n", TAM - i, numero[i]);
+    }
+    
+    return 0;
 }
 
