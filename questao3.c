44e90d935906dd6b1015785709a165b081dac8cf
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2024-04-22 19:14:35 -0300

    Update and rename questao3 to questao3.c

--- questao3.c
+++ questao3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main() {
     char nome[50];
@@ -15,9 +16,14 @@ int main() {
     scanf("%s", email);
     printf("Digite o telefone: ");
     scanf("%s", telefone);
+    
+    system("clear");
+    printf("Dados coletados com sucesso!\n");
+    sleep(3);
 
  
     do {
+        system("clear");
         printf("\nEscolha uma opcao:\n");
         printf("1 - Nome e idade\n");
         printf("2 - Nome e email\n");
@@ -29,24 +35,29 @@ int main() {
 
         switch (opcao) {
             case 1:
+            system("clear");
                 printf("Nome: %s\n", nome);
                 printf("Idade: %d\n", idade);
                 break;
             case 2:
+            system("clear");
                 printf("Nome: %s\n", nome);
                 printf("Email: %s\n", email);
                 break;
             case 3:
+            system("clear");
                 printf("Nome: %s\n", nome);
                 printf("Telefone: %s\n", telefone);
                 break;
             case 4:
+            system("clear");
                 printf("Nome: %s\n", nome);
                 printf("Idade: %d\n", idade);
                 printf("Email: %s\n", email);
                 printf("Telefone: %s\n", telefone);
                 break;
             case 0:
+            system("clear");
                 printf("Encerrando o programa...\n");
                 return 0; // Encerra o programa imediatamente
             default:
