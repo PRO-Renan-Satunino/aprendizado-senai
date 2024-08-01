43fa3f63abd1ff15a89a8dc5675e77a535707f9a
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2024-08-01 08:28:28 -0300

    Update Atvd1.java

--- JAVA/atvd1/src/main/java/com/mycompany/atvd1/Atvd1.java
+++ JAVA/atvd1/src/main/java/com/mycompany/atvd1/Atvd1.java
@@ -10,9 +10,27 @@ import java.util.Scanner;
  *
  * @author Aluno
  */
+
+/*Programa para uma loja que vende seus produtos em 5 prestações. Objetivo é mostrar o nome da loja como resultado e o valor das parcelas*/
 public class Atvd1 {
 
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        //declarando variaveis
+        Double valor, valorFinal;
         
+        //Pedindo valores
+        System.out.println("Digite o Valor da Compra: ");
+        valor = teclado.nextDouble();
+        
+        //operacao de prestacao
+        valorFinal = valor / 5;
+        
+        //Mostrando Resultados
+        System.out.println("==============================================");
+        System.out.println("               Mamão com Açúcar");
+        System.out.println("==============================================");
+        System.out.println("Valor inteiro: " + valor);
+        System.out.println("Valor das Parcelas: " + valorFinal);
     }
 }
